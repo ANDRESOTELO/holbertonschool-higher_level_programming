ce6e14ca9f06d4d8b6cfbebc6e694ff153770ce2
ANDRESOTELO <andresotelo2020@gmail.com>
2020-12-11 04:10:05 +0000

    Change sum variable name

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ import sys
 if __name__ == '__main__':
     n = len(sys.argv)  # length of argv counting program
     argv_index = sys.argv  # set argv arguments in an array
-    sum = 0
+    suma = 0
     for i in range(1, n):
-        sum = sum + int(argv_index[i])
-    print("{:d}".format(sum))
+        suma = suma + int(argv_index[i])
+    print("{:d}".format(suma))

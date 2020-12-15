cb31f0a4b6f7190dc9b3bd613d9cab5370570519
ANDRESOTELO <andresotelo2020@gmail.com>
2020-12-15 19:53:03 +0000

    unction that replaces an element of a list at a specific position (like in C)

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    count = len(my_list)
+    if idx < 0 and n < idx:
+        return my_list
+    for n in range(count):
+        if idx == n:
+            my_list[n] = element
+            return my_list

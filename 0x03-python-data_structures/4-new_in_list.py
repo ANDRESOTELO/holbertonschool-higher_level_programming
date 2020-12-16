0c5139f4ee9998d4e5368b56d649d2a6e61f3c6f
ANDRESOTELO <andresotelo2020@gmail.com>
2020-12-16 00:00:33 +0000

    Replaces a index without modify old copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    count = len(my_list)
-    new = my_list.copy()
-    if idx < 0 or idx > count:
-        return my_list
-    for n in range(count):
-        if n == idx:
-            new[n] = element
-            return new
+    if my_list:
+        count = len(my_list)
+        new = my_list.copy()
+        if idx < 0 or idx >= count:
+            return my_list
+        new[idxx] = element
+        return new

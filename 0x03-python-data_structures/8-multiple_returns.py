cf14dd011dd12dd57656f3ce1fac0bb0d0393fdb
ANDRESOTELO <andresotelo2020@gmail.com>
2020-12-16 04:36:28 +0000

    Validate if sentence exists

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple_return = (0, )
-    length = len(sentence)
-    first = sentence[0]
-    tuple_return = (tuple_return[0] + length, first)
-    return tuple_return
+    if sentence:
+        tuple_return = (0, )
+        length = len(sentence)
+        first = sentence[0]
+        tuple_return = (tuple_return[0] + length, first)
+        return tuple_return
+    return (len(sentence), None)

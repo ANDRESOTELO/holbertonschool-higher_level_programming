d1b13e1463e533e165b0d1f15626a5d6b66cae09
ANDRESOTELO <andresotelo2020@gmail.com>
2020-12-17 21:11:50 +0000

    Replaces all occurrences of elem by another in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    for i in range(len(my_list)):
-        if my_list[i] != 2:
-            new_list[i] = my_list[i]
-        else:
-            new_list[i] = 89
-    return new_list
+    if my_list:
+        new_list = my_list.copy()
+        for i in range(len(my_list)):
+            if my_list[i] != 2:
+                new_list[i] = my_list[i]
+            else:
+                new_list[i] = 89
+        return new_list

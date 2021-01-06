cda2bdbc122fa17a9da4593105de9d9940fb930f
ANDRESOTELO <andresotelo2020@gmail.com>
2021-01-06 02:25:01 +0000

    Area of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Program to define a square (based on 2-square.py)"""
+
+
+class Square:
+    """Class that defines a square"""
+
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size

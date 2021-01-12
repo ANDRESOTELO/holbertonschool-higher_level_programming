4acb2b39ef69f1f5b8bfdf58b32f808906fdf52d
ANDRESOTELO <andresotelo2020@gmail.com>
2021-01-12 18:26:49 +0000

    output correct

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -104,6 +104,4 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """Class method"""
-        width = size
-        height = size
-        return Rectangle(width, height)
+        return cls(size, size)

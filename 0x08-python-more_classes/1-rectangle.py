eecf30c71b3327d3a15430a7d1595b071f1079e3
ANDRESOTELO <andresotelo2020@gmail.com>
2021-01-12 00:53:25 +0000

    Define edge cases

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,18 +7,31 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialization"""
-        self.__width = width
-        self.__height = height
+
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
+
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+
+        
+    @property
+    def height(self):
+        """Get height value"""
+        return self.__height
 
     @property
     def width(self):
         """Get width value"""
         return self.__width
-
-    @property
-    def height(self):
-        """Get height value"""
-        return self.__height
 
     @height.setter
     def height(self, value):
@@ -32,7 +45,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """Set value"""
+        """Set height value"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:

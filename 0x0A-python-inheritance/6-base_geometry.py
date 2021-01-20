dc2cc2826002cc69d9c7cd49cc9254d41afa6cb1
ANDRESOTELO <andresotelo2020@gmail.com>
2021-01-20 03:09:10 +0000

    raise Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Program improve geometry"""
+
+
+class BaseGeometry:
+    """Define class BaseGeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")

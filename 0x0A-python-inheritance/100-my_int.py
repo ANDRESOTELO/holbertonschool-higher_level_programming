3b55e41f775e3531bd8cc3c4eb7645ff63e5747c
ANDRESOTELO <andresotelo2020@gmail.com>
2021-01-20 04:54:50 +0000

    Myinteger

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Class MyInt"""
+
+
+class MyInt(int):
+    """A class MyInt that inherits from int"""
+
+    def __eq__(self, other):
+        return self.num == other
+
+    def __ne__(self, other):
+        return not self.__eq__(other)

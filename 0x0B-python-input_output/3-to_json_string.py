2a9d8634a4f7b08a66f840b867ee14286210644b
ANDRESOTELO <andresotelo2020@gmail.com>
2021-01-21 01:55:22 +0000

    Object to JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""To JSON string"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Function that returns the JSON
+    representation of an object
+    (string)
+    """
+
+    json_string = json.dumps(my_obj)
+    return json_string

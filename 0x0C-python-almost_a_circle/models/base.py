39d1f003156fa02e4b789d3e4842611e71edaa42
ANDRESOTELO <andresotelo2020@gmail.com>
2021-01-25 19:14:11 +0000

    base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Base class module'''
+
+
+class Base:
+    '''
+    This class will be the "base" of all
+    other classes
+    '''
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''Class constructor that have id paramete'''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

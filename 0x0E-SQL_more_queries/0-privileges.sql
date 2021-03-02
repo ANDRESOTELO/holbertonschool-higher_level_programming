d2c5c4306d99c065b5d321f10bff07f3bc83213a
ANDRESOTELO <andresotelo2020@gmail.com>
2021-03-02 21:41:15 +0000

    Show privileges of a user

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Lists all privileges of MySQL users on localhost server
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2B'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;

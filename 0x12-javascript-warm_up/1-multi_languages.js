b22d14b197ac66f965489adac04f19e5c3466bd0
ANDRESOTELO <andresotelo2020@gmail.com>
2021-03-23 04:49:20 +0000

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let languages = 'C is fun';
+console.log(languages);
+languages = 'Python is cool';
+console.log(languages);
+languages = 'JavaScript is amazing';
+console.log(languages);

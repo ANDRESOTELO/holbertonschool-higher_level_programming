534109784ee3fa54eb832805a394eb6bd5b33a2b
ANDRESOTELO <andresotelo2020@gmail.com>
2021-03-24 04:29:52 +0000

    Square inherits from square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/us/bin/node
+
+const SquareSuper = require('./5-square');
+
+module.exports = class Square extends SquareSuper {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    let i = 0;
+    for (; i < this.height; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
+};

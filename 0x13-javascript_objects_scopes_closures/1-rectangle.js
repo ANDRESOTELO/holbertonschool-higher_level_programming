ec34b09aaf2245957107382f991163af616f6cd3
ANDRESOTELO <andresotelo2020@gmail.com>
2021-03-24 01:43:26 +0000

    Update constructor

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+class Rectangle {
+  /* Constructor with two arguments */
+  constructor (w, h) {
+    /* this refers to the current object the code is being written inside */
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;

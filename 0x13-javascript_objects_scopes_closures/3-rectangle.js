d2d8fa7c4bda35e09f6059938ac511054ed968b6
ANDRESOTELO <andresotelo2020@gmail.com>
2021-03-24 02:41:29 +0000

    Create method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  /* Method */
+  print () {
+    const charSquare = 'X';
+    let i = 0;
+    let j = 0;
+
+    for (; i < this.width; i++) {
+      for (; j < this.height; j++) {
+        console.log(charSquare.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Rectangle;

d4d3bde085717d1fddfd9705e8bbd21cd3b8e407
ANDRESOTELO <andresotelo2020@gmail.com>
2021-03-25 04:58:32 +0000

    Read and write file

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/* fs.readFileSync() method */
+/* inbuilt application programming interface of fs module which is used to read the file and return its content */
+
+/* Include fs module */
+const fs = require('fs');
+
+/* Calling the readFileSync() method to read file that is passed in argv */
+const data1 = fs.readFileSync(process.argv[2], { encoding: 'utf8', flag: 'r' });
+const data2 = fs.readFileSync(process.argv[3], { encoding: 'utf8', flag: 'r' });
+
+fs.writeFileSync(process.argv[4], data1 + data2);

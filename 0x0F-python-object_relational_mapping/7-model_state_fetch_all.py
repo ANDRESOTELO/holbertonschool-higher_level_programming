da76a08d85737d4e882dd5d20e70cf8897129604
ANDRESOTELO <andresotelo2020@gmail.com>
2021-03-28 00:56:34 +0000

    Create engine and session

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''The return value of create_engine() is an instance of Engine'''
+
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sys import argv
+
+'''Variables'''
+user_name = argv[1]
+password = argv[2]
+data_base = argv[3]
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user_name, password, data_base),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()

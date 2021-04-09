620cf9d0be7806f552f272840238fd9cbf0fcd5a
ANDRESOTELO <andresotelo2020@gmail.com>
2021-04-09 18:16:19 +0000

    FIXED output text

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-# Python script that fetches https://intranet.hbtn.io/status
+'''
+Python script that fetches https://intranet.hbtn.io/status
+'''
 
 import urllib.request
 
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
-print("Boby response:")
+print("Body response:")
 print("\t- type: {}".format(type(html)))
 print("\t- content: {}".format(html))
-print("\t- utf8 content: {}".format(html.decode()))
+print("\t- utf8 content: {}".format(html.decode('utf-8')))

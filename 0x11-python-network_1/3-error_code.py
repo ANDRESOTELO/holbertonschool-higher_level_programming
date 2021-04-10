27257bece11a38a28abb52a73572a78e6005e9aa
ANDRESOTELO <andresotelo2020@gmail.com>
2021-04-10 04:30:49 +0000

    ERROR CODE FIXED

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 '''
 Python script that takes in a URL, sends a request to the URL and
-displays the body of the response (decoded in utf-8)
+displays the body of the response (decoded in utf-8).
 '''
 
 import sys
 from urllib import request, error
 
+
 if __name__ == '__main__':
 
     try:
         with request.urlopen(sys.argv[1]) as response:
-            print(response.read().decode('utf8'))
-    except error.HTTPError as error:
-        print("Error code: {}".format(error.code))
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))

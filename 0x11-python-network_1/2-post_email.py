1c1b580c716cd2b9345b892452ec10568e51875f
ANDRESOTELO <andresotelo2020@gmail.com>
2021-04-10 03:12:45 +0000

    POST method

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+Write a Python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter, and displays
+the body of the response (decoded in utf-8)
+'''
+
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == '__main__':
+    try:
+        url = sys.argv[1]
+        # Data dictionary
+        header = {'email': sys.argv[2]}
+
+        data = parse.urlencode(header).encode()
+
+        # if data != null reference to method POST
+        with request.urlopen(url, data=data) as response:
+            print(response.read().decode('utf-8'))
+
+    except:
+        pass

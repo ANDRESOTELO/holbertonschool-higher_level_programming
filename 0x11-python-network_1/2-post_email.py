356c00a6dc5931ebc38f1ca34ccfc15ddef3dc26
ANDRESOTELO <andresotelo2020@gmail.com>
2021-04-10 03:39:24 +0000

    FIXED POST method

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,21 +6,17 @@ the body of the response (decoded in utf-8)
 '''
 
 import sys
-import urllib.request
-import urllib.parse
+from urllib import request, parse
 
 
 if __name__ == '__main__':
-    try:
-        url = sys.argv[1]
-        # Data dictionary
-        value = {'email': sys.argv[2]}
 
-        data = parse.urlencode(value).encode('utf8')
+    url = sys.argv[1]
+    # Data dictionary
+    value = {'email': sys.argv[2]}
 
-        # if data != null reference to method POST
-        with request.urlopen(url, data) as response:
-            print(response.read().decode('utf8'))
-        print('Your email is: '.format(sys.argv[2]))
-    except:
-        pass
+    data = parse.urlencode(value).encode('ascii')
+
+    # if data != null reference to method POST
+    with request.urlopen(url, data=data) as response:
+        print(response.read().decode('utf8'))

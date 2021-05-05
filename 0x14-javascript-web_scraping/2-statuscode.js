fa12abc0c0f03529a64062a2a8d4ecf1900b35ad
ANDRESOTELO <andresotelo2020@gmail.com>
2021-05-05 03:46:34 +0000

    ADDED | Fixed and handling error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-const url = require('url');
 const adr = process.argv;
 
-request(adr[2], function(error, response, body) {
-    console.log('code:', response && response.statusCode);
+request(adr[2], function (error, response, body) {
+  if (error) {
+    return console.log(error);
+  }
+  console.log('code:', response && response.statusCode);
 });

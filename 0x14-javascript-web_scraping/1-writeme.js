8f616bc7953d8cf8c0d217300bc497c890947c04
ANDRESOTELO <andresotelo2020@gmail.com>
2021-05-05 03:17:55 +0000

    ADDED | Write

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,10 +3,8 @@
 const fs = require('fs');
 const path = process.argv;
 
-fs.appendFile(path[2], path[3], 'utf8', function (err, data) {
+fs.writeFile(path[2], path[3], 'utf8', function (err, data) {
   if (err) {
     return console.error(err);
   }
-
-  console.log(data);
 });

25b788171cdde39183f34e6860b5d06600f8037a
ANDRESOTELO <andresotelo2020@gmail.com>
2021-05-12 23:03:11 +0000

    ADDED | jQuery change text of header

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+/*
+updates the text of the <header> element to 'New Header!!!' when
+the user clicks on #update_header
+*/
+$('#update_header').click(function () {
+  $('header').text('New Header!!!');
+});

aedd7baca2563219d5923e1e390b5bc7ed345756
ANDRESOTELO <andresotelo2020@gmail.com>
2021-05-12 20:58:14 +0000

    ADDED | JavaScript - Web jQuery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// to select tag from HTML and save it into a variable
+const textHeader = document.querySelector('header');
+
+// change the color of text based on tag <header>
+textHeader.style.color = '#FF0000';

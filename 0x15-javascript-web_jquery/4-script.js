d56f10b4a6123f145948b7683821e667d912d715
ANDRESOTELO <andresotelo2020@gmail.com>
2021-05-12 22:18:26 +0000

    ADDED | jQuery toggles class

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,7 @@
+/*
+toggles the class of the <header> element when the user clicks
+on the tag toggle_header
+*/
+$('#toggle_header').click(function () {
+  $('header').toggleClass('red green');
+});

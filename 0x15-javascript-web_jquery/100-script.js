60d34f24e83befe7f955f31517d1be8243cc5d0e
ANDRESOTELO <andresotelo2020@gmail.com>
2021-05-13 04:53:32 +0000

    ADDED | updates the text color of the <header>

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+/*
+Updates the text color of header
+*/
+document.onreadystatechange = function () {
+  if (document.readyState === 'complete') {
+    document.querySelector('header').style.color = '#FF0000';
+  }
+};
